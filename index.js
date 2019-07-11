ccd5e71feb99f56fea7ed384b99b6faeca0d5921
Honchar Denys <crackeraki@gmail.com>
2019-07-11 12:17:42 +0300

    initialize

---
+++ index.js
@@ -0,0 +1,23 @@
+var derer  = require('derer');
+module.exports = function(sd){
+	sd.derer = derer;
+	var dererOpts = {
+		varControls: ['{{{', '}}}']
+	}
+	if(!sd.config.production){
+		dererOpts.cache = false;
+	}
+	derer.setDefaults(dererOpts);
+	sd.app.engine('html', derer.renderFile);
+	sd.app.set('view engine', 'html');
+	sd.app.set('view cache', true);
+	derer.setFilter('string',function(input){
+		return input&&input.toString()||'';
+	});		
+	derer.setFilter('fixlink',function(link){
+		if(link.indexOf('//')>0) return link;
+		else return 'http://'+link;
+	});
+	// derer.setFilter('tr', sd._tr);
+	// derer.setFilter('translate', sd._tr);
+}
